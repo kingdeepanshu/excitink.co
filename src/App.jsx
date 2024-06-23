b4091e161a079ebef1864285bdd2fc4a78a88749
DevKaran <64530394+kingdeepanshu@users.noreply.github.com>
2024-06-23 17:00:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
     <div className="App">
       <Navbar/>
       <HeroPage/>
-      <Newarr />
-      <TopSelling/>
-      <Category/>
+{/*       <Newarr /> */}
+{/*       <TopSelling/> */}
+      // <Category/>
       {/* <Testimonials/> */}
       <Footer/>
     </div>
